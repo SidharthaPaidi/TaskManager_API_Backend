3bc715a221de2f7e807ecb66f9dad363e5931337
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-07-10 19:47:19 +0530

    Add the task Routes

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -9,12 +9,36 @@ import { Req } from '@nestjs/common';
 @Controller('task')
 
 export class TaskController {
-    constructor( private  taskService : TaskService) {}
+    constructor(private taskService: TaskService) { }
 
     @Post('/create')
     @UseGuards(JwtAuthGuard)
     async create(@Body('name') taskName: string, @Req() req) {
-      return await this.taskService.createTask(taskName, req.user);
+        return await this.taskService.createTask(taskName, req.user);
+    }
+
+    @Post('/get')
+    @UseGuards(JwtAuthGuard)
+    async get(@Req() req) {
+        return await this.taskService.getTasks();
+    }
+
+    @Post('/getById')
+    @UseGuards(JwtAuthGuard)
+    async getById(@Body('id') taskId: string) {
+        return await this.taskService.getTaskById(taskId);
+    }
+
+    @Post('/update')
+    @UseGuards(JwtAuthGuard)
+    async update(@Body('id') taskId: string, @Body('name') taskName: string, @Body('status') taskStatus: boolean) {
+        return await this.taskService.updateTask(taskId, taskName, taskStatus);
+    }
+
+    @Post('/delete')
+    @UseGuards(JwtAuthGuard)
+    async delete(@Body('id') taskId: string) {
+        return await this.taskService.deleteTask(taskId);
     }
 
 }
