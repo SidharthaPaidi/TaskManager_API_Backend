9123c4edf2f8ba92b6ab33be4a31e07d1ced2f8e
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-07-18 11:50:14 +0530

    enabled cors for frontend origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://task-manager-frontend-seven-rust.vercel.app/', // Your frontend URL
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
